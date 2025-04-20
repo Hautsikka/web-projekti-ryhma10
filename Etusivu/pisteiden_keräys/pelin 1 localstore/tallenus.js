12262ff8c411a309532e1df87da4ee5a4083eac7
arttuheikkila <arheikki24@students.oamk.fi>
2025-04-20 17:44:29 +0300

    localstoret peleille ja linkitys pisteet sivustolle

---
+++ Etusivu/pisteiden_keräys/pelin 1 localstore/tallenus.js
@@ -0,0 +1,5 @@
+// Tallennetaan localStorageen avaimella peli3_arttuH_pisteet
+localStorage.setItem("peli1_aleksi_pisteet", 10);
+
+// (Valinnainen) Konsoli-ilmoitus
+console.log("Pisteet tallennettu: 10");

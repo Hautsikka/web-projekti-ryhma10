8867a1d6253054c53e29b1654ff455745ae58cb6
arttuheikkila <arheikki24@students.oamk.fi>
2025-04-25 15:03:51 +0300

    korjauksia

--- Etusivu/pisteiden_keräys/java/pisteet.js
+++ Etusivu/pisteiden_keräys/java/pisteet.js
@@ -1,16 +1,19 @@
 // Haetaan pelin nimi HTML:n body-elementistä
 const pelinNimi = document.body.dataset.pelinNimi;
 
-// Pistelaskuri
-let pisteet = 0;
-
-// Pelaaja vastaa oikein
-function pelaajaVastasiOikein() {
-  pisteet += 1; // Lisää yksi piste
+// Simuloidaan pisteiden laskua (korvaa omalla logiikalla)
+function laskePisteet() {
+  // Tässä olisi sun oma logiikka, esim. laskurit, vastaukset jne.
+  // Esimerkki: satunnainen testipiste
+  return Math.floor(Math.random() * 10) + 1;
 }
 
 // Pisteiden tallennus
 function tallennaPisteet() {
+  const pisteet = laskePisteet();
   localStorage.setItem(`${pelinNimi}_pisteet`, pisteet);
   alert(`Pisteet tallennettu: ${pisteet}`);
 }
+
+// Kutsu tätä pelin lopussa, esim. kun käyttäjä klikkaa "Lopeta peli"
+tallennaPisteet();
---
+++ Etusivu/pisteiden_keräys/pelin1_local_store/tallenus.js
@@ -0,0 +1,5 @@
+// Tallennetaan localStorageen avaimella peli1_aleksi_pisteet
+localStorage.setItem("peli1_aleksi_pisteet", 10);
+
+// (Valinnainen) Konsoli-ilmoitus
+console.log("Pisteet tallennettu: 10");
